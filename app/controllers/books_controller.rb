642584fe943828a9bdcd3c8ce63e94cfbd4503cf
Hannah <hannahjoo24@gmail.com>
2020-04-28 13:16:43 -0700

    Updated books_controller/view/test files for show action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,7 @@
 BOOKS = [
-  { title: "Hidden Figures", author: "Margot Lee Shetterly"},
-  { title: "Practical Object-Oriented Design in Ruby", author: "Sandi Metz"},
-  { title: "Kindred", author: "Octavia E. Butler"}
+  { title: "Hidden Figures", author: "Margot Lee Shetterly", description: "Great book about Nasa pioneers"},
+  { title: "Practical Object-Oriented Design in Ruby", author: "Sandi Metz", description: "Classic Book"},
+  { title: "Kindred", author: "Octavia E. Butler", description: "A mystery"}
 ]
 
 
@@ -9,4 +9,18 @@ class BooksController < ApplicationController
   def index 
     @books = BOOKS
   end 
+
+  def show 
+    # raise
+    book_id = params[:id].to_i
+    @book = BOOKS[book_id]
+
+    if @book.nil?
+
+      head :not_found # it sends back an error code 404
+      return 
+    end 
+
+
+  end
 end
--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -5,17 +5,44 @@ describe BooksController do
   #   value(1+1).must_equal 2
   # end
 
-  it "responds to index" do 
-    get books_path
+  describe "index action" do 
+    it "should get index" do 
+      get books_path
+  
+      must_respond_with :success # 200 => nominal case 
+      
+      # must_respond_with :redirect # 3XX
+      # must_respond_with :missing # 404
+      # must_respond_with :error #4-5XX
+  
+      # must_redirect_to "/"
+      # must_redirect_to root_path #path name 
+      # must_redirect_to_controller: 'book', action: 'index'
+    end 
+  end
 
-    must_respond_with :success # 200 => nominal case 
-    
-    # must_respond_with :redirect # 3XX
-    # must_respond_with :missing # 404
-    # must_respond_with :error #4-5XX
+  describe "show action" do 
+    it "should get a valid book" do 
+      # Arrange 
+      valid_book_id = 1 
 
-    # must_redirect_to "/"
-    # must_redirect_to root_path #path name 
-    # must_redirect_to_controller: 'book', action: 'index'
-  end 
+      # Act 
+      get "/books/#{valid_book_id}" 
+      # get "/books/1"  # same as above (Arrange & Act)
+
+      # Assert 
+      must_respond_with :success  
+    end 
+
+    it "should respond with not_found (404) for invalid ids" do 
+      # Arrange 
+      invalid_book_id = 999 
+
+      # Act 
+      get "/books/#{invalid_book_id}"
+
+      # Assert 
+      must_respond_with :not_found
+    end
+  end
 end
