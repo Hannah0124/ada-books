60efe83cabcb2cbc70961bdf08f97fa99c371387
Hannah <hannahjoo24@gmail.com>
2020-04-28 12:05:49 -0700

    Update books controller, view, routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # verb 'path', to: 'controller#aciton'
+  get '/books', to: 'books#index'
+  # => When you receive a GET request to the /book path, go to the BooksController class and run the index action
 end
