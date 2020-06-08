a6c5bc8fe49ab9ea5a44330b772686863ed63a73
Hannah <hannahjoo24@gmail.com>
2020-06-07 23:05:38 -0700

    Added Omniauth initializer

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,7 @@
+# config/initializers/omniauth.rb
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :github, ENV["GITHUB_CLIENT_ID"], ENV["GITHUB_CLIENT_SECRET"], scope: "user:email"
+end
+
+
+# reference: https://github.com/Ada-Developers-Academy/textbook-curriculum/blob/master/09-intermediate-rails/oauth.md
